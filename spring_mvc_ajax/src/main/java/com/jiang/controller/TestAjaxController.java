ca23004fd5fea076f37ea8e7c633b7fdbe1173c1
jiangronghua <613870505@qq.com>
2022-12-31 10:55:50 +0800

    新增@RequestBody注解

--- spring_mvc_ajax/src/main/java/com/jiang/controller/TestAjaxController.java
+++ spring_mvc_ajax/src/main/java/com/jiang/controller/TestAjaxController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -19,6 +21,16 @@ import java.util.Map;
  * b> 在SpringMVC配置文件中设置   <mvc:annotation-driven />
  * c> 在处理请求的控制器方法的形参位置， 直接设置json格式的请求参数要转换的java类型的形参 使用@RequestBody标识即可
  * 3. RequestBody: 将所标识的控制器方法的返回值作为响应报文的响应体响应到浏览器
+ * 4. 使用@RequestBody注解响应浏览器json格式的数据
+ * a> 导入jackson的依赖
+ * b> 在SpringMVC配置文件中设置   <mvc:annotation-driven />
+ * c> 将需要转换为json字符串的java对象直接作为控制器方法的返回值，使用@ResponseBody注解标识控制器方法
+ * 就可以将Java对象直接转换为java字符串，并响应到浏览器
+ *
+ * 常用的Java对象转换为json的结果
+ * 实体类---> json对象
+ * map ---> json对象
+ * list---> json数组
  */
 @Controller
 public class TestAjaxController {
@@ -46,4 +58,30 @@ public class TestAjaxController {
     public String testResponseBody() {
         return "success";
     }
+
+    @RequestMapping("/test/ResponseBody/json")
+    @ResponseBody
+    public List<User> testResponseBodyJson() {
+        User user1 = new User(1001, "admin1", "123456", 23, "男");
+        User user2 = new User(1002, "admin2", "123456", 23, "男");
+        User user3 = new User(1003, "admin2", "123456", 23, "男");
+        List<User> list = Arrays.asList(user1, user2, user3);
+        return list;
+    }
+
+    /* public Map<String, Object> testResponseBodyJson() {
+        User user1 = new User(1001, "admin1", "123456", 23, "男");
+        User user2 = new User(1002, "admin2", "123456", 23, "男");
+        User user3 = new User(1003, "admin2", "123456", 23, "男");
+        Map<String, Object> map = new HashMap<>();
+        map.put("1001", user1);
+        map.put("1002", user2);
+        map.put("1003", user3);
+        return map;
+    } */
+
+    /* public User testResponseBodyJson() {
+        User user = new User(1001, "admin", "123456", 23, "男");
+        return user;
+    } */
 }
