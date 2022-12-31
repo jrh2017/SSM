852925ba861f311d2ef512b41b64aaf4d6200d4b
jiangronghua <613870505@qq.com>
2022-12-31 20:24:12 +0800

    多个过滤器的执行顺序

--- spring_mvc_extension/src/main/java/com/jiang/interceptor/SecondInterceptor.java
+++ spring_mvc_extension/src/main/java/com/jiang/interceptor/SecondInterceptor.java
@@ -9,31 +9,24 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author jiangronghua
- *
- * preHandle(): 在控制器方法执行之前执行，其返回值标识对控制器方法对拦截（false）或放行（true）
- * postHandle(): 在控制器方法执行之后执行
- * afterCompletion(): 在控制器方法执行之后，且渲染视图完毕之后执行
- *
- * 多个拦截器对执行顺序和在SpringMVC对配置文件中配置对顺序有关
- * preHandle()按照配置对属性执行，而postHandle()和afterCompletion()按配置对反序执行
  */
 @Component
-public class FirstInterceptor implements HandlerInterceptor {
+public class SecondInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println("FirstInterceptor -----> preHandle");
+        System.out.println("SecondInterceptor -----> preHandle");
         return HandlerInterceptor.super.preHandle(request, response, handler);
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        System.out.println("FirstInterceptor -----> postHandle");
+        System.out.println("SecondInterceptor -----> postHandle");
         HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        System.out.println("FirstInterceptor -----> afterCompletion");
+        System.out.println("SecondInterceptor -----> afterCompletion");
         HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
     }
 }
