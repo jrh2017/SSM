1a1dfa4f3006053092122a522f2c18b7a0d6f688
jiangronghua <613870505@qq.com>
2022-12-31 21:04:57 +0800

    通过注解配置异常处理

---
+++ spring_mvc_extension/src/main/java/com/jiang/controller/ExceptionController.java
@@ -0,0 +1,21 @@
+package com.jiang.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * @author jiangronghua
+ */
+// 将当前类标识为异常处理组件
+@ControllerAdvice
+public class ExceptionController {
+
+    // 设置要处理的异常信息
+    @ExceptionHandler(ArithmeticException.class)
+    public String handleException(Throwable ex, Model model){
+        // ex表示控制器方法所出现的异常
+        model.addAttribute("ex", ex);
+        return "error";
+    }
+}
