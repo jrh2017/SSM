7aff2683442a77ad0083e53552c4de8d02113602
jiangronghua <613870505@qq.com>
2022-12-31 18:36:46 +0800

    新增上传功能

--- spring_mvc_ajax/src/main/java/com/jiang/controller/FileUpAndDownController.java
+++ spring_mvc_ajax/src/main/java/com/jiang/controller/FileUpAndDownController.java
@@ -6,18 +6,47 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 /**
  * @author jiangronghua
+ *
+ * ResponseEntity: 可以作为控制器方法的返回值， 标识响应到浏览器到完整的响应报文
+ *
+ * 文件上传的要求：
+ * 1. form表单的请求方式必须是post
+ * 2. form表单必须设置属性enctype="multipart/form-data"
  */
 @Controller
 public class FileUpAndDownController {
+
+    @RequestMapping("/test/up")
+    public String testUp(MultipartFile photo, HttpSession session) throws IOException {
+        // 获取上传的文件的文件名
+        String filename = photo.getOriginalFilename();
+        // 获取ServletContext对象
+        ServletContext servletContext = session.getServletContext();
+        // 获取当前工程下photo目录的真实目录
+        String photoPath = servletContext.getRealPath("photo");
+        // 创建photoPath所对应的File对象
+        File file = new File(photoPath);
+        // 判断file对所对应目录是否存在
+        if (!file.exists()) {
+            file.mkdir();
+        }
+        String finalPath = photoPath + File.separator + filename;
+        // 上传文件
+        photo.transferTo(new File(finalPath));
+        System.out.println("filename:" + filename);
+        return "success";
+    }
 
     @RequestMapping("/test/down")
     public ResponseEntity<byte[]> testResponseEntity(HttpSession session) throws
