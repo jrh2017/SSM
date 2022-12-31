2537c8967f2a51684ff3cddfe8ff6f0227af2b85
Jiangronghua <613870505@qq.com>
2022-12-31 22:59:37 +0800

    注解配置SpringMVC
    完成通过使用注解方式配置SpringMVC项目并成功运行

---
+++ spring_mvc_annotation/src/main/java/com/jiang/config/SpringConfig.java
@@ -0,0 +1,11 @@
+package com.jiang.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author jiangronghua
+ */
+// 将类标识为配置类
+@Configuration
+public class SpringConfig {
+}
---
+++ spring_mvc_annotation/src/main/java/com/jiang/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.jiang.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author jiangronghua
+ */
+@Controller
+public class TestController {
+}
